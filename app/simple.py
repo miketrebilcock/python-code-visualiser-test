dc78124d40bb1af9efe3f387ab6660c72a9ac73d
Mike <m.trebilcock@saltosystems.com>
2024-09-06 22:48:41 +0100

    Add Test Code

---
+++ app/simple.py
@@ -0,0 +1,10 @@
+class Simple:
+    def __init__(self):
+        self.x = 1
+        self.y = 2
+
+    def add(self):
+        return self.x + self.y
+    
+    def sub(self):
+        return self.x - self.y
